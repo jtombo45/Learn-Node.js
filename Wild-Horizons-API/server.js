f63b7e74ba1af6e655f11d0e2d9fd20db846ef1b
Judicael Tombo <jtombo@deloitte.com>
2025-11-17 17:31:09 -0600

    JSON and API

--- Wild-Horizons-API/server.js
+++ Wild-Horizons-API/server.js
@@ -1,20 +1,8 @@
-import http from 'node:http';
+const animals = {
+  type: 'cattle',
+  name: 'Bessie',
+  age: 4,
+  weight: 1500,
+}
 
-const PORT = 8000;
-
-
-// Store the server instance in a variable
-const server = http.createServer((req, res) => {
-  if (req.url === '/api') {
-    res.writeHead(204); 
-    res.write('This is some data \n');
-    res.write('This is some more data \n');
-    // End the response is required to send the data to the client
-    res.end('Hello, Wild Horizons API!', 'utf-8', () => console.log('Response end') ); // This message appears in the browser
-  }
-});
-
-// Start the server and listen on the specified port
-server.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`); // This message appears in the terminal
-});
+console.log(typeof JSON.stringify(animals)); // "string"
